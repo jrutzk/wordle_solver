696553eb33dd04cf068c2578cee8e9818b6954e5
jrutzk <jrutzky@live.com>
2025-12-16 21:51:10 -0600

    Add query for GUESSES_POSITIONS_VIEW
    Add a new SQL query to select from GUESSES_POSITIONS_VIEW.

--- queries.sql
+++ queries.sql
@@ -23,3 +23,20 @@ ORDER BY
 	COUNT(*) DESC;
 
 -- CRANE STOUP DIMLY
+SELECT
+	*
+FROM
+	wordle.GUESSES_POSITIONS_VIEW
+WHERE
+	1 = 1
+	--AND P1 = ''
+	--and p1 not in (select letter from wordle.letters)
+	--and p2 = ''
+	--and p2 not in (select letter from wordle.letters)
+	--and p3 = ''
+	--and p3 not in (select letter from wordle.letters)
+	--and p4 = ''
+	--and p4 not in (select letter from wordle.letters)
+	--AND P5 = ''
+	--and p5 not in (select letter from wordle.letters)
+	;
