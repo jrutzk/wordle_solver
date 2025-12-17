4cf07d19d3dd5173093e18408834c28bca9d938d
jrutzk <jrutzky@live.com>
2025-12-17 11:18:44 -0600

    Add filtering query for wordle answers
    Added a SELECT query to filter answers based on green and yellow letters.

--- queries.sql
+++ queries.sql
@@ -44,3 +44,27 @@ WHERE
 insert into grey values ('');
 insert into yellow values ('');
 insert into green values ( , '');
+
+SELECT
+	*
+FROM
+	wordle.ANSWERS_POSITIONS_VIEW
+WHERE
+	1 = 1
+	AND P1 = (select letter from green where position = 1)
+	AND P2 = (select letter from green where position = 2)
+	AND P3 = (select letter from green where position = 3)
+	AND P4 = (select letter from green where position = 4)
+	AND P5 = (select letter from green where position = 5)
+	and p1 not in (select letter from wordle.grey)
+	and p2 not in (select letter from wordle.grey)
+	and p3 not in (select letter from wordle.grey)
+	and p4 not in (select letter from wordle.grey)
+	and p5 not in (select letter from wordle.grey)
+	and (select letter from yellow) in (p1, p2, p3, p4, p5)
+	and (select letter from yellow) in (p1, p2, p3, p4, p5)
+	and (select letter from yellow) in (p1, p2, p3, p4, p5)
+	and (select letter from yellow) in (p1, p2, p3, p4, p5)
+	and (select letter from yellow) in (p1, p2, p3, p4, p5)
+	--
+	;
