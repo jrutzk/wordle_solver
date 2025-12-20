13bf69dc43698b9c72565a14ca1d94b5c891e30f
jrutzk <jrutzky@live.com>
2025-12-20 15:50:55 -0600

    Grant truncate privileges on wordle tables
    Added truncate privileges for specific tables in the wordle schema.

--- grants.sql
+++ grants.sql
@@ -2,3 +2,7 @@ GRANT USAGE ON SCHEMA wordle TO app_user;
 GRANT INSERT, SELECT, UPDATE, DELETE ON ALL TABLES IN SCHEMA wordle TO app_user;
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA wordle TO app_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA wordle GRANT INSERT, SELECT ON TABLES TO app_user;
+
+grant truncate on table wordle.grey to app_user;
+grant truncate on table wordle.green to app_user;
+grant truncate on table wordle.yellow to app_user;
